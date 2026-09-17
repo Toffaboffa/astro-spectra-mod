7771db42bd97ec8b4aa94612fc0069ba2eb9f589
Kristoffer Åberg <87949165+Toffaboffa@users.noreply.github.com>
2026-09-17 09:19:44 +0200

    Bump SPECTRA PRO version to 2.1.1

--- docs/frontend/scripts/mod/uiTweaksV203.js
+++ docs/frontend/scripts/mod/uiTweaksV203.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const sp = global.SpectraPro = global.SpectraPro || {};
-  const VERSION = 'v2.1.0';
+  const VERSION = 'v2.1.1';
   let initialStripeCentered = false;
 
   function updateVersionBadge() {
