a624438d6af92c29d5162de24823bff43f6ae425
Kristoffer Åberg <87949165+Toffaboffa@users.noreply.github.com>
2026-09-18 15:39:52 +0200

    Bump SPECTRA PRO version to v2.2.6

--- docs/frontend/scripts/mod/uiTweaksV203.js
+++ docs/frontend/scripts/mod/uiTweaksV203.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const sp = global.SpectraPro = global.SpectraPro || {};
-  const VERSION = 'v2.2.5';
+  const VERSION = 'v2.2.6';
   let initialStripeCentered = false;
   let helpClickBound = false;
   let helpPausedByHelp = false;
