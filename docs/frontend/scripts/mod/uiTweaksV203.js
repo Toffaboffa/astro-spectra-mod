cef49c124c51b68253bdcfc87757dd9cb361665f
Kristoffer Åberg <87949165+Toffaboffa@users.noreply.github.com>
2026-09-21 17:31:43 +0200

    chore: show SPECTRA PRO v2.3.4

--- docs/frontend/scripts/mod/uiTweaksV203.js
+++ docs/frontend/scripts/mod/uiTweaksV203.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const sp = global.SpectraPro = global.SpectraPro || {};
-  const VERSION = 'v2.3.3';
+  const VERSION = 'v2.3.4';
   let initialStripeCentered = false;
   let helpClickBound = false;
   let helpPausedByHelp = false;
