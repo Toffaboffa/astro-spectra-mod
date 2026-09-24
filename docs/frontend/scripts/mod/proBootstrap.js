ae4fa7d84ca5d63d68af15d6a5adbd73b1e383e2
Kristoffer Åberg <87949165+Toffaboffa@users.noreply.github.com>
2026-09-24 13:38:37 +0200

    Bump proBootstrap.js to v3.1.0

--- docs/frontend/scripts/mod/proBootstrap.js
+++ docs/frontend/scripts/mod/proBootstrap.js
@@ -238,7 +238,7 @@ function ensureHost() {
         },
         loadedAt: Date.now(),
         scaffold: false,
-        version: '3.0.9'
+        version: '3.1.0'
       };
     } else {
       const mods = v15.registry.modules || (v15.registry.modules = {});
@@ -247,7 +247,7 @@ function ensureHost() {
       });
       v15.registry.loadedAt = v15.registry.loadedAt || Date.now();
       v15.registry.scaffold = false;
-      v15.registry.version = '3.0.9';
+      v15.registry.version = '3.1.0';
     }
     return v15.registry;
   }
