e61848982f5dbbe416ce1694caadd2fae289afbb
Kristoffer Åberg <87949165+Toffaboffa@users.noreply.github.com>
2026-09-24 17:28:50 +0200

    Bump exampleSpectrumUi.js to v3.1.4

--- docs/frontend/scripts/mod/exampleSpectrumUi.js
+++ docs/frontend/scripts/mod/exampleSpectrumUi.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const sp = global.SpectraPro = global.SpectraPro || {};
-  const VERSION = '3.1.3';
+  const VERSION = '3.1.4';
   const BUTTON_ID = 'spLoadExampleBtn';
   const OVERLAY_ID = 'spExampleChooserOverlay';
   const STYLE_ID = 'spExampleChooserStyle';
