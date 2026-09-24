c87996f3e9ea75ae5dcc6657e5c9d0ae7542aec9
Kristoffer Åberg <87949165+Toffaboffa@users.noreply.github.com>
2026-09-24 15:39:49 +0200

    Bump uiPanels.js to v3.1.2

--- docs/frontend/scripts/mod/uiPanels.js
+++ docs/frontend/scripts/mod/uiPanels.js
@@ -1,7 +1,7 @@
 (function (global) {
   'use strict';
   const sp = global.SpectraPro = global.SpectraPro || {};
-  const VERSION = 'v3.1.1';
+  const VERSION = 'v3.1.2';
   sp.version = VERSION;
 
   function createModeTabs(container) {
